c94976fda97a749e4816b0360ac1f2c99c611033
MD NAEEM ISLAM <md.sn.naeem786@gmail.com>
2025-11-10 21:50:35 +0600

    add 4 api service create, fetch all, fetch one, and fetch top-rated for homepage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -27,6 +27,32 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const db = client.db('HomeMateDB')
+    const servicesCollection = db.collection('Services')
+
+    //add one services
+    app.post('/services', async(req, res) =>{
+        const data = req.body
+        const result = await servicesCollection.insertOne(data);
+        res.send(result)
+    })
+    //get all services
+    app.get('/services', async(req, res) =>{
+        const result = await servicesCollection.find().toArray();
+        res.send(result)
+    })
+    //get one services
+    app.get('/services/:id', async(req, res) =>{
+        const id = req.params
+        const result = await servicesCollection.findOne({_id: new ObjectId(id)})
+        res.send(result)
+    })
+    // get top rating services
+    app.get('/top-rating', async (req, res) => {
+      const result = await servicesCollection.find().sort({rating: 'descending'}).limit(6).toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(" Connected to MongoDB");
   } catch (err) {
@@ -35,7 +61,7 @@ async function run() {
 }
 
 run().catch(console.dir);
-
+       
 // Test route
 app.get("/", (req, res) => {
   res.send("The server is running");
